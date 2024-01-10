f1fc357233c6c2c42aba989aed4b7c7f67112827
박수민 <95015832+Ssuminn@users.noreply.github.com>
2024-01-10 17:48:55 +0900

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,15 @@
+package Assignment;
+
+class Menu {
+    static int idSeq = 1;
+
+    int id;
+    String name;		// 메뉴 이름
+    String description;	// 메뉴 설명
+
+    Menu(String name, String description) {
+        this.id = idSeq++;
+        this.name = name;
+        this.description = description;
+    }
+}
